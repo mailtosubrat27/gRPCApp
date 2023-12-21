312c391157c00fde5eb089fcbfac2e583c6671e3
subrat.sahu <subrat.sahu@BRILAPNB5575.BRILLIO.COM>
2023-12-21 21:20:36 +0530

    File Read

---
+++ grpcDemo/src/main/java/com/grpc/FileReadWrite.java
@@ -0,0 +1,25 @@
+package com.grpc;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import com.gen.models.Person;
+
+public class FileReadWrite {
+
+	public static void main(String[] args) throws IOException {
+		Person person = Person.newBuilder()
+				.setName("raj")
+				.setAge(10)
+				.build();
+		Path path = Paths.get("Person-1");
+		Files.write(path, person.toByteArray());
+		
+		byte[] bytes= Files.readAllBytes(path);
+		System.out.println(Person.parseFrom(bytes));
+
+	}
+
+}
