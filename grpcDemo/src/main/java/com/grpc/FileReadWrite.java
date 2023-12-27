1923908268249cbbc89b7da31f723d5be9e4aee6
subrat.sahu <subrat.sahu@BRILAPNB5575.BRILLIO.COM>
2023-12-27 23:15:24 +0530

    changes

--- grpcDemo/src/main/java/com/grpc/FileReadWrite.java
+++ grpcDemo/src/main/java/com/grpc/FileReadWrite.java
@@ -10,15 +10,15 @@ import com.gen.models.Person;
 public class FileReadWrite {
 
 	public static void main(String[] args) throws IOException {
-		Person person = Person.newBuilder()
-				.setName("raj")
-				.setAge(10)
-				.build();
+//		Person person = Person.newBuilder()
+//				.setName("raj")
+//				.setAge(10)
+//				.build();
 		Path path = Paths.get("Person-1");
-		Files.write(path, person.toByteArray());
+//		Files.write(path, person.toByteArray());
 		
 		byte[] bytes= Files.readAllBytes(path);
-		System.out.println(Person.parseFrom(bytes));
+		System.out.println(Person.parseFrom(bytes).getGender());
 
 	}
 
